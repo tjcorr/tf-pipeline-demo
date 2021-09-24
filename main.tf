dc78f151483857a6076548d4dacbf1eaf7e8c316
T.J. Corrigan <tj.corrigan@gmail.com>
2021-09-24 17:40:38 +0000

    remove owner for tf

--- main.tf
+++ main.tf
@@ -15,9 +15,7 @@ provider "azurerm" {
   features {}
 }
 
-provider "github" {
-  owner = "tjcorr"
-}
+provider "github" {}
 
 resource "azurerm_resource_group" "rg-aks" {
   name     = var.resource_group_name
