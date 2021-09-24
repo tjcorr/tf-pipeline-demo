a502acee071228812058f06921ebb1925404b4cb
T.J. Corrigan <tj.corrigan@gmail.com>
2021-09-24 16:11:58 +0000

    generate gh repo secret using tf

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 2.62"
+    },
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
     }
   }
 }
@@ -11,7 +15,19 @@ provider "azurerm" {
   features {}
 }
 
+provider "github" {}
+
 resource "azurerm_resource_group" "rg-aks" {
   name     = var.resource_group_name
   location = var.location
+}
+
+data "github_repository" "repo" {
+  full_name = "tjcorr/tf-pipeline-demo"
+}
+
+resource "github_actions_secret" "example_secret" {
+  repository       = data.github_repository.repo.full_name
+  secret_name      = "tf-sample-secret"
+  plaintext_value  = "super_sekret"
 }
