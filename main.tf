ff34d126870da14a880d0e1772bb716b736d9ac7
T.J. Corrigan <tjcorr@github.com>
2022-06-08 21:45:52 +0000

    update backend

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "rg-tf-pipeline-demo-state"
-    storage_account_name = "tfpipelinedemostate"
+    storage_account_name = "tfpipelinedemo"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
