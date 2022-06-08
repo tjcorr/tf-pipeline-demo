0d6138d3f48a9aa91b8f7099b0f5a87960aa664a
T.J. Corrigan <tjcorr@github.com>
2022-06-08 21:40:49 +0000

    add use-oidc to backend

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ terraform {
     storage_account_name = "tfpipelinedemostate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    use_oidc             = true
   }
 }
 
