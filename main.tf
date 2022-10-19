ac7775f747e6826fde7053656614504ca3f29f7e
T.J. Corrigan <tjcorr@github.com>
2022-10-19 14:27:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ terraform {
     }
   }
 
+  # Update this block with the location of your terraform state file
   backend "azurerm" {
     resource_group_name  = "rg-tf-pipeline-demo-state"
     storage_account_name = "tfpipelinedemo"
@@ -20,6 +21,7 @@ provider "azurerm" {
   use_oidc = true
 }
 
+# Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
 resource "azurerm_resource_group" "rg-aks" {
   name     = var.resource_group_name
   location = var.location
